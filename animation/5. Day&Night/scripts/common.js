7ddd5bd7eb8dc0405728e1fb29d97f5a746e64df
OhHyeonJu0415 <guswn2209@gmail.com>
2023-08-03 09:54:02 +0900

    chore : 공통 변수 선언

--- animation/5. Day&Night/scripts/common.js
+++ animation/5. Day&Night/scripts/common.js
@@ -8,3 +8,11 @@ export const nightTitle = document.getElementById("textNight");
 
 //background
 export const turn = document.getElementById("turn");
+
+//day animation
+export const cloud = document.getElementById("cloud");
+export const airPlane = document.getElementById("airPlane");
+
+//night animation
+export const ufo = document.getElementById("ufo");
+export const vim = document.getElementById("vim");
